24ec8c6d3bca68ad88d16734415a43a48be8c70b
Jacob Prud'homme <jacob.pkmn@gmail.com>
2017-11-30 18:16:08 -0500

    Renamed Blocks to Block
    For grammatical consistency

---
+++ block.h
@@ -0,0 +1,19 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <string>
+#include <iostream>
+
+class Block {
+  public:
+    virtual void moveLeft() = 0;
+    virtual void moveRight() = 0;
+    virtual void moveDown() = 0;
+
+    virtual void clockwise() = 0;
+    virtual void antiClockwise() = 0;
+
+    virtual std::string prettyprint() = 0;
+};
+
+#endif
