aa33e0568a2a15b96872e8e2248295c8bd4a2a64
Jacob Prud'homme <jacob.pkmn@gmail.com>
2017-12-01 13:42:27 -0500

    Updated TextDisplay header
    Sectioned off different parts of text display

--- textdisplay.h
+++ textdisplay.h
@@ -1,10 +1,13 @@
 #ifndef TEXTDISPLAY_H
 #define TEXTDISPLAY_H
 
+#include <string>
 #include "observer.h"
 
 class TextDisplay : public Observer {
-  std::vector<std::vector<char>> theDisplay;
+  std::vector<string> scoreDisp;
+  std::vector<std::vector<char>> gridDisp;
+  std::vector<char> upcomingDisp;
 
   public:
     void notify(Subject &whoFrom) override;
