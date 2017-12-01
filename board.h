f226d19f56c29bf5122fece047128ac7359549de
Jacob Prud'homme <jacob.pkmn@gmail.com>
2017-12-01 18:39:20 -0500

    Added Karan's board and cell files

--- board.h
+++ board.h
@@ -5,21 +5,23 @@
 #include <cstddef>
 #include "display.h"
 #include "cell.h"
-#include "score.h"
 
 
 class Board {
   std::vector<std::vector<Cell>> theBoard;  // The actual grid.
   TextDisplay *td = nullptr; // The text display.
   Observer *ob = nullptr;  // Another observer (intent:  graphics)
+  int rows;
+  int cols;
+  int numBlock;
 
  public:
   ~Board();
 
   void setObserver(Observer *ob);
   void init(int rows, int columns); // Sets up an width x height grid.  Clears old grid, if necessary.
-  std::vector<int> whichRowFull(); //checks if any row is full and returns all the rows number
-                                  //updates scores in this, Also keep track of the moving down
+  void whichRowFullDelete(); //checks if any row is full and delet all the rows and move everything
+                             //downwards
   bool isFull() const;
   void notify(Subject &whoFrom); //the block is sent to make changes to the entire grid
 
--- cell.h
+++ cell.h
@@ -11,23 +11,24 @@ class Cell {
   int r; //which row is this at
   int c; //which column is it at
   bool containsBlock; //does it contain a block or not
-  std::string block; //tracking which type of block we have right now i, j, l......
+  std::string block; //tracking which type of block we have right now i, j, l...... "N" for not any block
   int numBlock; //for scoring, to see if we add score in the right way
   int level; //which level are we on, for calculating scores
   //I may be wrong, edit how you feel or I think we should have all this in state
 
 public:
-  Cell(int r, int c, bool b, std::string str, int n, int l);
+  Cell(int r, int c, bool b, std::string block, int n, int l);
 
   bool isBlock(); // returns true if the cell contains block or not
-  void notify(Block &whoFrom) override;// My neighbours will call this
+  void toggle(); // changes the containsblock clock switch
+  void notify(Subject &whoFrom) override;// My neighbours will call this
                                                 // when they've changed state
-  int getX();
-  int getY();
-  int getbool();
+  int getR();
+  int getC();
   std::string getStr();
   int getBlockNum();
   int getLevel();
-  void setbool(bool isBlock);
+  void setBlockNum(int n);
+  void setbool(bool x);
 };
 #endif
