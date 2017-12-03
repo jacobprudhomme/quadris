95992867450c462caf7ef7163878157464f92fa0
mikytron123 <vyasmiky@gmail.com>
2017-12-02 22:32:13 -0500

    Update blockselector.cc
    added code for level 1

--- blockselector.cc
+++ blockselector.cc
@@ -45,4 +45,39 @@ void Blockselector::runlevel(){
         }
         Upcoming::Instance()->setNextBlocks() = allblocks; 
     }
+     }else if(getlevel() == 1){
+        int prob = (srand(seed) % 100);
+        int next = (srand(seed) % 100);
+        if(prob<=8.3333333){
+            SBlock();
+        }else if(prob>8.333333 && prob<=16.6666666){
+            ZBlock();
+        }else if(prob>16.6666666 && prob<= 33.333333){
+            IBLock();
+        }else if(prob>33.333333 && prob<=50){
+            JBlock();
+        }else if(prob>50 && prob<=66.666666){
+            LBlock();
+        }else if(prob>66.6666666 && prob <=83.33333333){
+            OBlock();
+        }else{
+            TBlock();
+        }
+        
+         if(next<=8.3333333){
+              Upcoming::Instance()->setNextBlocks() = 'S'; 
+            
+        }else if(next>8.333333 && next<=16.6666666){
+             Upcoming::Instance()->setNextBlocks() = 'Z'; 
+        }else if(next>16.6666666 && next<= 33.333333){
+             Upcoming::Instance()->setNextBlocks() = 'I'; 
+        }else if(next>33.333333 && next<=50){
+             Upcoming::Instance()->setNextBlocks() = 'J'; 
+        }else if(next>50 && next<=66.666666){
+             Upcoming::Instance()->setNextBlocks() = 'L'; 
+        }else if(next>66.6666666 && next <=83.33333333){
+             Upcoming::Instance()->setNextBlocks() = 'O'; 
+        }else{
+             Upcoming::Instance()->setNextBlocks() = 'T'; 
+        }
 }
