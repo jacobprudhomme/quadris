58ad28ee2d3a70faa1f4b93a3caa0aedaa6379fd
Miky Vyas <vyasmiky@gmail.com>
2017-12-04 17:17:22 -0500

    main fixed

--- main.cc
+++ main.cc
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+
+
 #include "score.h"
 #include "upcoming.h"
 #include "board.h"
@@ -15,6 +17,8 @@
 #include "zblock.h"
 #include "sblock.h"
 #include "tblock.h"
+#include "blockselector.h"
+
 
 using namespace std;
 
@@ -22,213 +26,222 @@ using namespace std;
 Score *Score::singleton_instance = 0;
 Upcoming *Upcoming::singleton_instance = 0;
 
+static void inputHelper(Board *obj, Block *b, string command, int level, int &i,
+                         bool usingseed, int seed) {
+    bool changelvl = false;
+
+    const string left = "lef";//left(lef)
+    const string right = "ri";//right(ri)
+    const string down = "do";//down(do)
+    const string cw = "cl";//clockwise(cl)
+    const string ccw = "co";//counterclockwise(co)
+    const string drop = "dr";//drop(dr)
+    const string levelup = "levelu";//levelup(levelu)
+    const string leveldown = "leveld";//leveldown(leveld)
+    const string norandom = "n";//norandom(n)
+    const string random = "ra";//random(ra)
+    const string sequence = "s"; //sequence fiel(s)
+    const string restart = "re";//restart(re)
+    const string hint = "h";//hint(h)
+ //   cout<<"the command is "<<command;
+ //   cout<<"comapring this is"<<command.compare(0,3,right);
+    if (command.compare(0,3,left) == 1) {
+        b->moveLeft();
+    } else if (command.compare(0,3,right)== 1) {
+
+//	    cout<<"moving right";
+        b->moveRight();
+    } else if (command.compare(0,2,down)== 1) {
+        b->moveDown();
+    } else if (command.compare(0,2,cw) == 1) {
+        b->clockwise();
+    } else if (command.compare(0,2,ccw)== 1 ) {
+        b->antiClockwise();
+    } else if (command.compare(0,2,drop) == 1) {
+        while(obj->getDown()){
+            b->moveDown();
+        }
+
+        if (changelvl) {
+            changelvl = false;
+
+            Upcoming::instance()->setlevel(level);
+          //  v = Upcoming::instance()->getVecBlock();
+            Upcoming::instance()->updatenextblock(level, seed, usingseed, obj);
+        }
+
+        ++i;
+        b = Upcoming::instance()->getVecBlock().at(i);
+        b->init();
+    } else if (command.compare(0,6,levelup) == 1) {
+        level++;
+
+        if (level >= 4) {
+            level = 4;
+        }
+
+        changelvl = true;
+    } else if (command.compare(0,6,leveldown) == 1) {
+        level--;
+
+        if (level <= 0) {
+            level = 0;
+        }
+
+        changelvl = true;
+    } else if (command.compare(0,1,norandom) == 1) {
+        string file;
+        cin >> file;
+        //usingrandom = false;
+    } else if (command.compare(0,2,random) == 1) {
+        //usingrandom = true;
+    } else if (command.compare(0,1,sequence) == 1) {
+        string seqfile;
+        cin >> seqfile;
+        ifstream f{seqfile};
+
+        string commands;
+
+        while (f >> commands) {
+            istringstream ss{commands};
+            int mult;
+
+            if (ss >> mult) {
+                ss >> command;
+                for (int i = 0; i < mult; i++) {
+                    inputHelper(obj, b, command, level, i, usingseed, seed);
+                }
+            } else {
+                ss >> command;
+                inputHelper(obj, b, command, level, i, usingseed, seed);
+            }
+        }
+    } else if (command.compare(0,2,restart) == 1) {
+        //restart grid
+    } else if (command.compare(0,1,hint) == 1) {
+        //hint
+    } else if (command.compare(0,1,"I") == 1) {
+        //insert I block
+    } else if (command.compare(0,1,"J") == 1) {
+        //insert J block
+    } else if (command.compare(0,1,"O") == 1) {
+        //insert O block
+    } else if (command.compare(0,1,"S") == 1) {
+        //insert S block
+    } else if (command.compare(0,1,"Z") == 1) {
+        //insert Z block
+    } else if (command.compare(0,1,"T") == 1) {
+        //insert T block
+    } else if (command.compare(0,1,"L") == 1) {
+        //insert L block
+    }
+}
 
 int main(int argc, char *argv[]) {
-    int level = 0;
-    bool text = false;
+    // PROCESS COMMAND LINE FLAGS
+    //bool text = false;
     bool usingseed = false;
-    int seed;
-    bool fileused = false;
-    string filename;
-    bool usingrandom = true;
-/*
-    for(int x = 1; x < argc; ++x){
-        if (argv[x] == "-text"){
-             text = true;
-        } else if (argv[x] == "-seed"){
-            istringstream iss {argv[x+1]};
-            if (!(iss>>seed)){
-                "Please enter a correct seed";
+    int seed = -1;
+    //bool fileused = false;
+    string filename = "sequence.txt";
+    int level = 0;
+
+    for (int x = 1; x < argc; ++x) {
+        if (string(argv[x]), "-text") {
+            //text = true; //using textdisplay
+        } else if (string(argv[x]) == "-seed") {
+            istringstream ss{argv[x+1]};
+            if (!(ss >> seed)) {
+                cerr << "Please enter a correct seed" << endl;
                 return 1;
             }
+
             usingseed = true;
-            ++x;
-        } else if (argv[x] == "-scriptfile" ){
-            fileused = true;
-                filename = argv[x+1];
-            ++x;
-        } else if (argv[x] == "-startlevel"){
-            istringstream iss {argv[x+1]};
-            if (!(iss>>level)){
-                "Please enter a correct level";
+        } else if (string(argv[x]) == "-scriptfile" ) {
+            //fileused = true;
+            filename = string(argv[x+1]);
+        } else if (string(argv[x]) == "-startlevel") {
+            istringstream ss{argv[x+1]};
+            if (!(ss>>level)) {
+                cerr << "Please enter a correct level" << endl;
                 return 1;
             } else if (level > 4 || level < 0) {
-                "Please enter a valid level";
+                cerr << "Please enter a valid level" << endl;
+                return 1;
             }
-            ++x;
         }
     }
-*/
 
-    char mult;
-    string left = "lef";//left(lef)
-    string right = "ri";//right(ri)
-    string down = "do";//down(do)
-    string cw = "cl";//clockwise(cl)
-    string ccw = "co";//counterclockwise(co)
-    string drop = "dr";//drop(dr)
-    string levelup = "levelu";//levelup(levelu)
-    string leveldown = "leveld";//leveldown(leveld)
-    string norandom = "n";//norandom(n)
-    string random = "ra";//random(ra)
-    string sequence = "s"; //sequence fiel(s)
-    string restart = "re";//restart(re)
-    string hint = "h";//hint(h)
-    string s;
-    string file;
-    string seqfile;
 
-    Board *obj = new Board{18, 11, 0};
+
+    // INITIALIZE BOARD AND OTHERS
+    Board *obj = new Board{18, 11, -1};
     obj->init();
-    IBlock i(obj);
-    cout << (*obj) << endl;
-    for(int x = 0; x < 16; ++x) {
-        i.moveDown();
-    }
-    for(int x = 0; x < 11; x++) {
-        i.moveRight();
-    }
-    IBlock r(obj);
-    for(int x = 0; x < 16; ++x) {
-        r.moveDown();
-    }
-    for(int x = 0; x < 3; x++) {
-        r.moveRight();
-    }
-    JBlock j(obj);
-    for(int x = 0; x < 16; ++x) {
-        j.moveDown();
-    }
-    cout << (*obj) << endl;  
-    /*
-    while (cin >> s) {
-        istringstream iss{s};
-        iss >> mult;
+    Blockselector *bsl = new Blockselector{filename, level};
+    bsl->runlevel(obj);
+    Upcoming::instance()->updatenextblock(level, seed, usingseed, obj);
+   // Upcoming::instance()->updatenextblock(level,seed,usingseed,obj);
+  //  cout<<Upcoming::instance()->getVecBlock().size();
+   
+    // PRINT OUT INITIAL EMPTY BOARD
+   // vector<Block *> v{Upcoming::instance()->getVecBlock()};
+  //  cout << *obj;
+//    cout<<Upcoming::instance()->getVecBlock().size();
+    
 
-        if (s.compare(0,3,left) == 0) {
-           //move left
-        } else if (s.compare(0,2,right) == 0) {
-          //move right
-        } else if (s.compare(0,2,down) == 0) {
-            //move down
-        } else if (s.compare(0,2,cw) == 0) {
+        Upcoming::instance()->setlevel(level);
+//	    cout<<Upcoming::instance()->getVecBlock().size();
 
-        } else if (s.compare(0,2,ccw) == 0) {
+     //   v = Upcoming::instance()->getVecBlock();
+//	    cout<<Upcoming::instance()->getVecBlock().size();
+       
+   bool firstblock = true;
+             Block *b = Upcoming::instance()->getVecBlock().at(0);
+            if(firstblock){
+             firstblock = false;
+	     b->init();
+	    }
+           cout<<*obj;
 
-        } else if (s.compare(0,2,drop) == 0) {
 
-        } else if (s.compare(0,6,levelup) == 0) {
-            if (mult >= 0) {
-              level += mult;
+
+    // START COMMAND LOOP
+    string s;
+ //  while (cin >> s) {
+ //       istringstream ss{s};
+
+//        Upcoming::instance()->setlevel(level);
+  //      v = Upcoming::instance()->getVecBlock();
+
+        int i = 0;
+  //  Upcoming::instance()->getVecBlock().size();
+      cout<<"first loop";
+        while(i < int(Upcoming::instance()->getVecBlock().size())) {
+//	    cout<<Upcoming::instance()->getVecBlock().size();
+      cout<<"second loop";
+       //   Block *b = Upcoming::instance()->getVecBlock().at(i);
+       //     if(firstblock){
+       //      firstblock = false;
+	//     b->init();
+	//    }
+           cout<<*obj;
+        while(cin >> s){
+	istringstream ss{s};	
+            int mult;
+            string command;
+            if (ss >> mult) {
+                ss >> command;
+		cout<<"this is command before" << command;
+                for (int j = 0; j < mult; j++) {
+                    inputHelper(obj, b, command, level, i, usingseed, seed);
+                }
+            } else {
+                
+		cout<<"this is command befeor" << s;
+                inputHelper(obj, b, s, level, i, usingseed, seed);
             }
 
-            if (level >= 4) {
-              level = 4;
-            }
-
-        } else if (s.compare(0,6,leveldown) == 0) {
-            if (mult >= 0) {
-                level -= mult;
-            }
-
-            if (level <= 0) {
-                level = 0;
-            }
-        } else if (s.compare(0,1,norandom) == 0) {
-          cin >> file;
-          usingrandom = false;
-        } else if (s.compare(0,2,random) == 0) {
-            usingrandom = true;
-        } else if (s.compare(0,1,sequence) == 0) {
-            cin >> seqfile;
-            ifstream f{seqfile};
-
-            string commands;
-
-            while (f>>commands) {
-              istringstream isss{commands};
-              while (isss >> s) {
-
-                  if (iss >> mult) {
-
-                  }
-
-                  if (s.compare(0,3,left) == 0) {
-                      //move left
-                  } else if (s.compare(0,2,right) == 0) {
-                      //move right
-                  } else if (s.compare(0,2,down) == 0) {
-                      //move down
-                  } else if (s.compare(0,2,cw) == 0) {
-
-                  } else if (s.compare(0,2,ccw) == 0) {
-
-                  } else if (s.compare(0,2,drop) == 0) {
-
-                  } else if (s.compare(0,6,levelup) == 0) {
-                      if (mult >= 0) {
-                          level += mult;
-                      }
-
-                      if (level >= 4) {
-                          level = 4;
-                      }
-                  } else if (s.compare(0,6,leveldown) == 0) {
-                      if (mult >= 0) {
-                          level -= mult;
-                      }
-
-                      if (level <= 0) {
-                          level = 0;
-                      }
-                  } else if (s.compare(0,1,norandom) == 0) {
-                      cin >> file;
-                      usingrandom = false;
-                  } else if (s.compare(0,2,random) == 0) {
-                      usingrandom = true;
-                  } else if (s.compare(0,2,restart) == 0) {
-
-                  } else if (s.compare(0,1,hint) == 0) {
-
-                  } else if (s.compare(0,1,"I") == 0) {
-
-                  } else if (s.compare(0,1,"J") == 0) {
-
-                  } else if (s.compare(0,1,"O") == 0) {
-
-                  } else if (s.compare(0,1,"S") == 0) {
-
-                  } else if (s.compare(0,1,"Z") == 0) {
-
-                  } else if (s.compare(0,1,"T") == 0) {
-
-                  } else if (s.compare(0,1,"L") == 0) {
-
-                  }
-              }
-            }
-
-
-        } else if (s.compare(0,2,restart) == 0) {
-              //restart grid
-        } else if (s.compare(0,1,hint) == 0) {
-             //hint
-        } else if (s.compare(0,1,"I") == 0) {
-            //insert I block
-        } else if (s.compare(0,1,"J") == 0) {
-               //insert J block
-        } else if (s.compare(0,1,"O") == 0) {
-            //insert O block
-        } else if (s.compare(0,1,"S") == 0) {
-            //insert S block
-        } else if (s.compare(0,1,"Z") == 0) {
-             //insert Z block
-        } else if (s.compare(0,1,"T") == 0) {
-             //insert T block
-        } else if (s.compare(0,1,"L") == 0) {
-               //insert L block
+            cout << *obj;
         }
-
     }
-     */
 }
